04b998a4d1ba74b77fafaa5058b8e3caba3be7ae
pmieszczak <piotr.mieszczak@syzygy.pl>
2022-05-05 19:39:37 +0200

    feat: 🎸 Added SideNav and Header components
    Basic layout

---
+++ src/app/layout/sidenav/sidenav.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SidenavComponent } from './sidenav.component';
+
+@NgModule({
+  declarations: [SidenavComponent],
+  imports: [CommonModule],
+})
+export class SidenavModule {}
