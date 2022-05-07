445ef2ce625f8b985c86436df3d7dfbaa8a0c3b0
pmieszczak <piotr.mieszczak@syzygy.pl>
2022-05-07 23:27:14 +0200

    test: 💍 PostsListService http tests

---
+++ src/app/modules/posts-list/state/posts-list.service.spec.ts
@@ -0,0 +1,53 @@
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { environment } from '../../../../environments/environment';
+import { createServiceFactory, SpectatorService } from '@ngneat/spectator/jest';
+import { PostsListService } from './posts-list.service';
+import { PostsListStore } from './posts-list.store';
+import { PostsListModule } from '../posts-list.module';
+
+const mockPostData = [
+  {
+    userId: 1,
+    id: 1,
+    title: 'title 1',
+    body: 'body 1',
+  },
+];
+
+describe('GetData on ngOnInit', () => {
+  let spectator: SpectatorService<PostsListService>;
+  let postsListService: PostsListService;
+  let postsListStore: PostsListStore;
+  let httpController: HttpTestingController;
+
+  const createService = createServiceFactory({
+    service: PostsListService,
+    providers: [PostsListStore],
+    imports: [PostsListModule, HttpClientTestingModule],
+  });
+
+  beforeEach(() => {
+    spectator = createService();
+    postsListService = spectator.inject(PostsListService);
+    postsListStore = spectator.inject(PostsListStore);
+    httpController = spectator.inject(HttpTestingController);
+  });
+
+  it('should get all data', function () {
+    const url = environment.apiUrl + '/posts';
+
+    spectator.service.getAll().subscribe((res) => {
+      expect(res).toEqual(mockPostData);
+    });
+
+    const req = httpController.expectOne({
+      method: 'GET',
+      url: `${url}`,
+    });
+
+    req.flush(mockPostData);
+  });
+});
